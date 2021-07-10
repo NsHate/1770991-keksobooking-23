25259a58d762cf24b4e903a104f474cdc619e97d
Nikita <nikitasysoev51@gmail.com>
2021-07-10 20:18:26 +0300

    Добавляет функционал по дз 8.1

--- js/const.js
+++ js/const.js
@@ -1,5 +1,4 @@
 export const MIN_ELEMENT = 1;
-
 export const MIN_PRICE = {
   flat: 1000,
   bungalow: 0,
@@ -8,6 +7,7 @@ export const MIN_PRICE = {
   hotel: 3000,
 };
 export const MAX_PRICE = 1000000;
-
 export const MAX_DECIMAL_NUMBERS = 5;
-
+export const MIN_TITLE_LENGTH = 30;
+export const MAX_TITLE_LENGTH = 100;
+export const MAX_ROOMS = 100;
